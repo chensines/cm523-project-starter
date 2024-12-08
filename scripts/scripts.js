100c39c07305211a47480bcf25f1275ee2762777
chensines <chens777@bu.edu>
2024-12-07 19:08:10 -0500

    .

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -26,9 +26,32 @@ $(document).ready(function () {
                 $('#nextBtn1').on('click', function () {
                   $('.slick-slider').slick('slickGoTo',3);
                 })
+                $('#prevBtn2').on('click', function () {
+                  $('.slick-slider').slick('slickGoTo',0);
+                })
+                $('#nextBtn2').on('click', function () {
+                  $('.slick-slider').slick('slickGoTo',3);
+                })
             });
         });
       });
     });
   });
+  
+  
+    // 监听 Slider 1 的跳转
+    $('.container:first-of-type').on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+      if (nextSlide === 2) { // 如果目标是跳到页面 3
+        $('.container:nth-of-type(2)').hide(); // 隐藏页面 2
+        $('.container:last-of-type').show(); // 显示页面 3
+      } else {
+        $('.container:nth-of-type(2)').show(); // 如果不是，恢复页面 2 的可见性
+      }
+    });
+  
+    // 如果需要直接在页面加载时隐藏某些 Slider
+    $('.container:nth-of-type(2)').hide();
+  
 
+    console.log('Event triggered!');
+console.log($('.container:nth-of-type(2)')); // 验证选择器
