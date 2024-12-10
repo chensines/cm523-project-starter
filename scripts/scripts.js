aa80115fe9f4ecde3ee619bd1e3327e737e33962
chensines <chens777@bu.edu>
2024-12-10 07:17:19 -0500

    .

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -19,6 +19,8 @@ $(document).ready(function () {
       speed: 500,
       slidesToShow: 1,
       slidesToScroll: 1,
+      vertical: true, // Use vertical scrolling
+      verticalSwiping: true,
       responsive: [
         {
           breakpoint: 400,
